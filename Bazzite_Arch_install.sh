8328c38847ffb6b89eb9403412793860efd6c42f
Ripplingsnake12 <mrcgibb@live.co.uk>
2024-12-19 18:56:33 +0100

    Update Bazzite_Arch_install.sh

--- Bazzite_Arch_install.sh
+++ Bazzite_Arch_install.sh
@@ -2,8 +2,8 @@
 # Script to install Bazzite Arch kernel, run additional setup, and update systemd configuration for MCU Powersave
 
 # Step 1: Install Bazzite Arch Kernel
-wget https://github.com/hhd-dev/kernel-bazzite/releases/download/6.11.6-304/linux-bazzite-6.11.6.304.bazzite.fc41-1-x86_64.pkg.tar.zst
-sudo pacman -U linux-bazzite-6.11.6.304.bazzite.fc41-1-x86_64.pkg.tar.zst
+wget https://github.com/hhd-dev/kernel-bazzite/releases/download/6.12.5-202/linux-bazzite-6.12.5.201.bazzite.fc41-1-x86_64.pkg.tar.zst
+sudo pacman -U linux-bazzite-6.12.5.201.bazzite.fc41-1-x86_64.pkg.tar.zst
 sudo pacman -R inputplumber 
 # Step 2: Run additional setup command
 curl -L https://github.com/hhd-dev/hhd/raw/master/install.sh | bash
