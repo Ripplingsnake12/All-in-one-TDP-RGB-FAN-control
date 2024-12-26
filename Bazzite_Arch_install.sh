8a81de043c991a44faee5f7ddbd2984ba55316cf
Ripplingsnake12 <mrcgibb@live.co.uk>
2024-12-26 21:18:35 +0100

    Powercontrol.sh

--- Bazzite_Arch_install.sh
+++ Bazzite_Arch_install.sh
@@ -1,19 +1,18 @@
+# Additional setup, and update systemd configuration for MCU Powersave
 
-# Script to install HHD , run additional setup, and update systemd configuration for MCU Powersave
+curl -L https://github.com/SteamDeckHomebrew/decky-installer/releases/latest/download/install_release.sh | sh
+curl -L https://raw.githubusercontent.com/mengmeet/PowerControl/main/install.sh | sh
+curl -L https://raw.githubusercontent.com/honjow/huesync/main/install.sh | sh
 
 
 sudo pacman -S discover flatpak mesa-git
+
 # Step 2: Run additional setup command
+
 curl -L https://github.com/hhd-dev/hhd/raw/master/install.sh | bash
 
 curl https://raw.githubusercontent.com/FakeMichau/fgmod/main/prepare.sh | bash -- 
 
-3: Update the existing override.conf file with necessary environment variables
-
-CONFIG_FILE="/usr/lib/systemd/system/hhd@.service.d/override.conf"
-
-echo "[Service]
-Environment=\"HHD_ALLY_POWERSAVE=1\"" | sudo tee $CONFIG_FILE
 
 # Mask power profile daemon
 
