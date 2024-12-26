bc3a3ff6e7911956ab1dd3383940936f3c05354e
Ripplingsnake12 <mrcgibb@live.co.uk>
2024-12-26 15:53:24 +0100

    Update Bazzite_Arch_install.sh

--- Bazzite_Arch_install.sh
+++ Bazzite_Arch_install.sh
@@ -1,10 +1,7 @@
 
-# Script to install Bazzite Arch kernel, run additional setup, and update systemd configuration for MCU Powersave
+# Script to install HHD , run additional setup, and update systemd configuration for MCU Powersave
 
-# Step 1: Install Bazzite Arch Kernel
-wget https://github.com/hhd-dev/kernel-bazzite/releases/download/6.12.5-204/linux-bazzite-6.12.5.204.bazzite.fc41-1-x86_64.pkg.tar.zst
-sudo pacman -U linux-bazzite-6.12.5.204.bazzite.fc41-1-x86_64.pkg.tar.zst
-sudo pacman -R inputplumber 
+
 sudo pacman -S discover flatpak mesa-git
 # Step 2: Run additional setup command
 curl -L https://github.com/hhd-dev/hhd/raw/master/install.sh | bash
@@ -16,9 +13,7 @@ curl https://raw.githubusercontent.com/FakeMichau/fgmod/main/prepare.sh | bash -
 CONFIG_FILE="/usr/lib/systemd/system/hhd@.service.d/override.conf"
 
 echo "[Service]
-Environment=\"HHD_PPD_MASK=1\"
-Environment=\"HHD_ALLY_POWERSAVE=1\"
-Environment=\"HHD_HORI_STEAM=1\"" | sudo tee $CONFIG_FILE
+Environment=\"HHD_ALLY_POWERSAVE=1\"" | sudo tee $CONFIG_FILE
 
 # Mask power profile daemon
 
