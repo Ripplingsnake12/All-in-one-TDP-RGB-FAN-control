6152acb8f516c115d21497c13fcf8b8ba687c4ba
Ripplingsnake12 <mrcgibb@live.co.uk>
2025-01-23 12:37:31 +0100

    Update asusctl.sh

--- asusctl.sh
+++ asusctl.sh
@@ -2,3 +2,20 @@ sudo pacman inputplumber asusctl rog-control-center discover flatpak
 
 paru -S mkinitcpio-firmware
 
+# A script to download and install steam-powerbuttond
+
+# Clone the repository
+git clone https://github.com/ShadowBlip/steam-powerbuttond.git
+
+# Navigate to the directory
+cd steam-powerbuttond
+
+# Run the install script
+./install.sh
+
+
+# Step 2: Install DLSS Enabler
+
+
+curl https://raw.githubusercontent.com/FakeMichau/fgmod/main/prepare.sh | bash -- 
+
